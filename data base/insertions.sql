1c87b187f9fec29fc7a47f63c624bfc5972e29a1
jarvanux <jhonjaider100015@gmail.com>
2017-12-27 13:44:02 -0500

    ...

--- data base/insertions.sql
+++ data base/insertions.sql
@@ -68,3 +68,47 @@ INSERT INTO `impacto` (`k_id_impacto`,`n_descripcion`) VALUES (2,'Alto');
 INSERT INTO `impacto` (`k_id_impacto`,`n_descripcion`) VALUES (3,'Significativo');
 INSERT INTO `impacto` (`k_id_impacto`,`n_descripcion`) VALUES (4,'Bajo');
 INSERT INTO `impacto` (`k_id_impacto`,`n_descripcion`) VALUES (5,'Insignificante');
+
+
+-- Volcando datos para la tabla matriz_riesgo.ref_combox: ~7 rows (aproximadamente)
+/*!40000 ALTER TABLE `ref_combox` DISABLE KEYS */;
+INSERT INTO `ref_combox` (`k_id_combox`, `n_value`, `n_text`, `n_table`, `n_sql`, `d_created_at`) VALUES
+	(1, 'k_id_riesgo', 'n_riesgo', 'riesgo', NULL, '2017-12-19 11:09:40'),
+	(2, 'k_id_factor_riesgo', 'n_descripcion', 'factor_riesgo', NULL, '2017-12-19 11:34:01'),
+	(3, 'k_id_probabilidad', 'n_descripcion', 'probabilidad', NULL, '2017-12-19 11:42:14'),
+	(4, 'k_id_impacto', 'n_descripcion', 'impacto', NULL, '2017-12-19 11:49:17'),
+	(5, 'k_id_plataforma', 'n_nombre', 'plataforma', NULL, '2017-12-19 11:53:24'),
+	(6, 'k_id_control', 'n_descripcion', 'control', NULL, '2017-12-19 12:07:00'),
+	(7, 'k_id_tipo_evento_1', 'n_descripcion', 'tipo_evento_1', NULL, '2017-12-20 10:06:05'),
+	(8, 'k_id_zona_geografica', 'n_nombre', 'zona_geografica', NULL, '2017-12-27 10:25:11');
+/*!40000 ALTER TABLE `ref_combox` ENABLE KEYS */;
+
+-- Volcando datos para la tabla matriz_riesgo.ref_probabilidad_impacto: ~25 rows (aproximadamente)
+/*!40000 ALTER TABLE `ref_probabilidad_impacto` DISABLE KEYS */;
+INSERT INTO `ref_probabilidad_impacto` (`k_⁯id_ref`, `k_id_probabilidad`, `k_id_impacto`, `n_calificacion`, `n_color`, `n_text_color`) VALUES
+	(1, 5, 5, 'Bajo', '#ADFF2F', '#000000'),
+	(2, 5, 4, 'Bajo', '#ADFF2F', '#000000'),
+	(3, 5, 3, 'Moderado', '#0000FF', '#FFFFFF'),
+	(4, 5, 2, 'Alto', '#FFFF00', '#000000'),
+	(5, 5, 1, 'Extremo', '#FF0000', '#FFFFFF'),
+	(6, 4, 5, 'Bajo', '#ADFF2F', '#000000'),
+	(7, 4, 4, 'Bajo', '#ADFF2F', '#000000'),
+	(8, 4, 3, 'Moderado', '#0000FF', '#FFFFFF'),
+	(9, 4, 2, 'Alto', '#FFFF00', '#000000'),
+	(10, 4, 1, 'Extremo', '#FF0000', '#FFFFFF'),
+	(11, 3, 5, 'Bajo', '#ADFF2F', '#000000'),
+	(12, 3, 4, 'Moderado', '#0000FF', '#FFFFFF'),
+	(13, 3, 3, 'Moderado', '#0000FF', '#FFFFFF'),
+	(14, 3, 2, 'Alto', '#FFFF00', '#000000'),
+	(15, 3, 1, 'Extremo', '#FF0000', '#FFFFFF'),
+	(16, 2, 5, 'Bajo', '#ADFF2F', '#000000'),
+	(17, 2, 4, 'Moderado', '#0000FF', '#FFFFFF'),
+	(18, 2, 3, 'Alto', '#FFFF00', '#000000'),
+	(19, 2, 2, 'Alto', '#FFFF00', '#000000'),
+	(20, 2, 1, 'Extremo', '#FF0000', '#FFFFFF'),
+	(21, 1, 5, 'Bajo', '#ADFF2F', '#000000'),
+	(22, 1, 4, 'Moderado', '#0000FF', '#FFFFFF'),
+	(23, 1, 3, 'Alto', '#FFFF00', '#000000'),
+	(24, 1, 2, 'Extremo', '#FF0000', '#FFFFFF'),
+	(25, 1, 1, 'Extremo', '#FF0000', '#FFFFFF');
+/*!40000 ALTER TABLE `ref_probabilidad_impacto` ENABLE KEYS */;
--- data base/matriz_riesgo_actualizada.sql
+++ data base/matriz_riesgo_actualizada.sql
@@ -25,11 +25,7 @@ CREATE TABLE IF NOT EXISTS `actividad` (
   PRIMARY KEY (`k_id_actividad`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.actividad: ~0 rows (aproximadamente)
-DELETE FROM `actividad`;
-/*!40000 ALTER TABLE `actividad` DISABLE KEYS */;
-/*!40000 ALTER TABLE `actividad` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.calificacion
 DROP TABLE IF EXISTS `calificacion`;
 CREATE TABLE IF NOT EXISTS `calificacion` (
@@ -48,13 +44,9 @@ CREATE TABLE IF NOT EXISTS `calificacion` (
   `total_calificacion` int(11) DEFAULT NULL,
   `niveles_disminuye` int(11) DEFAULT NULL,
   PRIMARY KEY (`k_id_calificacion`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.calificacion: ~0 rows (aproximadamente)
-DELETE FROM `calificacion`;
-/*!40000 ALTER TABLE `calificacion` DISABLE KEYS */;
-/*!40000 ALTER TABLE `calificacion` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.causa
 DROP TABLE IF EXISTS `causa`;
 CREATE TABLE IF NOT EXISTS `causa` (
@@ -65,13 +57,9 @@ CREATE TABLE IF NOT EXISTS `causa` (
   PRIMARY KEY (`k_id_causa`),
   KEY `k_id_riesgo_especifico` (`k_id_riesgo_especifico`),
   CONSTRAINT `FK_causa_riesgo_especifico` FOREIGN KEY (`k_id_riesgo_especifico`) REFERENCES `riesgo_especifico` (`k_id_riesgo_especifico`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.causa: ~0 rows (aproximadamente)
-DELETE FROM `causa`;
-/*!40000 ALTER TABLE `causa` DISABLE KEYS */;
-/*!40000 ALTER TABLE `causa` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.control
 DROP TABLE IF EXISTS `control`;
 CREATE TABLE IF NOT EXISTS `control` (
@@ -94,11 +82,7 @@ CREATE TABLE IF NOT EXISTS `control` (
   PRIMARY KEY (`k_id_control`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.control: ~0 rows (aproximadamente)
-DELETE FROM `control`;
-/*!40000 ALTER TABLE `control` DISABLE KEYS */;
-/*!40000 ALTER TABLE `control` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.control_especifico
 DROP TABLE IF EXISTS `control_especifico`;
 CREATE TABLE IF NOT EXISTS `control_especifico` (
@@ -117,91 +101,45 @@ CREATE TABLE IF NOT EXISTS `control_especifico` (
   CONSTRAINT `fk_fk_ce_causa` FOREIGN KEY (`k_id_causa`) REFERENCES `causa` (`k_id_causa`),
   CONSTRAINT `fk_fk_ce_control` FOREIGN KEY (`k_id_control`) REFERENCES `control` (`k_id_control`),
   CONSTRAINT `fk_fk_ce_factor_riesgo` FOREIGN KEY (`k_id_factor_riesgo`) REFERENCES `factor_riesgo` (`k_id_factor_riesgo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.control_especifico: ~0 rows (aproximadamente)
-DELETE FROM `control_especifico`;
-/*!40000 ALTER TABLE `control_especifico` DISABLE KEYS */;
-/*!40000 ALTER TABLE `control_especifico` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.factor_riesgo
 DROP TABLE IF EXISTS `factor_riesgo`;
 CREATE TABLE IF NOT EXISTS `factor_riesgo` (
   `k_id_factor_riesgo` int(11) NOT NULL AUTO_INCREMENT,
   `n_descripcion` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`k_id_factor_riesgo`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.factor_riesgo: ~7 rows (aproximadamente)
-DELETE FROM `factor_riesgo`;
-/*!40000 ALTER TABLE `factor_riesgo` DISABLE KEYS */;
-INSERT INTO `factor_riesgo` (`k_id_factor_riesgo`, `n_descripcion`) VALUES
-	(1, 'Recurso Humano'),
-	(2, 'Tecnológico'),
-	(3, 'Infraestructura Física'),
-	(4, 'Procesos'),
-	(5, 'Información'),
-	(6, 'Evento Externo - Condiciones Natural '),
-	(7, 'Evento Externo - Terceros');
-/*!40000 ALTER TABLE `factor_riesgo` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.impacto
 DROP TABLE IF EXISTS `impacto`;
 CREATE TABLE IF NOT EXISTS `impacto` (
   `k_id_impacto` int(11) NOT NULL AUTO_INCREMENT,
   `n_descripcion` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`k_id_impacto`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.impacto: ~5 rows (aproximadamente)
-DELETE FROM `impacto`;
-/*!40000 ALTER TABLE `impacto` DISABLE KEYS */;
-INSERT INTO `impacto` (`k_id_impacto`, `n_descripcion`) VALUES
-	(1, 'Extremo'),
-	(2, 'Alto'),
-	(3, 'Significativo'),
-	(4, 'Bajo'),
-	(5, 'Insignificante');
-/*!40000 ALTER TABLE `impacto` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.plataforma
 DROP TABLE IF EXISTS `plataforma`;
 CREATE TABLE IF NOT EXISTS `plataforma` (
   `k_id_plataforma` int(11) NOT NULL AUTO_INCREMENT,
   `n_nombre` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`k_id_plataforma`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.plataforma: ~5 rows (aproximadamente)
-DELETE FROM `plataforma`;
-/*!40000 ALTER TABLE `plataforma` DISABLE KEYS */;
-INSERT INTO `plataforma` (`k_id_plataforma`, `n_nombre`) VALUES
-	(1, 'a'),
-	(2, 'b'),
-	(3, 'c'),
-	(4, 'd'),
-	(5, 'e');
-/*!40000 ALTER TABLE `plataforma` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.probabilidad
 DROP TABLE IF EXISTS `probabilidad`;
 CREATE TABLE IF NOT EXISTS `probabilidad` (
   `k_id_probabilidad` int(11) NOT NULL AUTO_INCREMENT,
   `n_descripcion` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`k_id_probabilidad`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.probabilidad: ~5 rows (aproximadamente)
-DELETE FROM `probabilidad`;
-/*!40000 ALTER TABLE `probabilidad` DISABLE KEYS */;
-INSERT INTO `probabilidad` (`k_id_probabilidad`, `n_descripcion`) VALUES
-	(1, 'Alta'),
-	(2, 'Frecuente'),
-	(3, 'Probable'),
-	(4, 'Ocasional'),
-	(5, 'Inferior');
-/*!40000 ALTER TABLE `probabilidad` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.ref_combox
 DROP TABLE IF EXISTS `ref_combox`;
 CREATE TABLE IF NOT EXISTS `ref_combox` (
@@ -212,21 +150,9 @@ CREATE TABLE IF NOT EXISTS `ref_combox` (
   `n_sql` varchar(300) DEFAULT NULL,
   `d_created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`k_id_combox`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.ref_combox: ~7 rows (aproximadamente)
-DELETE FROM `ref_combox`;
-/*!40000 ALTER TABLE `ref_combox` DISABLE KEYS */;
-INSERT INTO `ref_combox` (`k_id_combox`, `n_value`, `n_text`, `n_table`, `n_sql`, `d_created_at`) VALUES
-	(1, 'k_id_riesgo', 'n_riesgo', 'riesgo', NULL, '2017-12-19 11:09:40'),
-	(2, 'k_id_factor_riesgo', 'n_descripcion', 'factor_riesgo', NULL, '2017-12-19 11:34:01'),
-	(3, 'k_id_probabilidad', 'n_descripcion', 'probabilidad', NULL, '2017-12-19 11:42:14'),
-	(4, 'k_id_impacto', 'n_descripcion', 'impacto', NULL, '2017-12-19 11:49:17'),
-	(5, 'k_id_plataforma', 'n_nombre', 'plataforma', NULL, '2017-12-19 11:53:24'),
-	(6, 'k_id_control', 'n_descripcion', 'control', NULL, '2017-12-19 12:07:00'),
-	(7, 'k_id_tipo_evento_1', 'n_descripcion', 'tipo_evento_1', NULL, '2017-12-20 10:06:05');
-/*!40000 ALTER TABLE `ref_combox` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.ref_probabilidad_impacto
 DROP TABLE IF EXISTS `ref_probabilidad_impacto`;
 CREATE TABLE IF NOT EXISTS `ref_probabilidad_impacto` (
@@ -235,44 +161,15 @@ CREATE TABLE IF NOT EXISTS `ref_probabilidad_impacto` (
   `k_id_impacto` int(11) DEFAULT '0',
   `n_calificacion` varchar(15) DEFAULT NULL,
   `n_color` varchar(7) NOT NULL DEFAULT '#FFFFFF',
+  `n_text_color` varchar(7) NOT NULL DEFAULT '#000000',
   PRIMARY KEY (`k_⁯id_ref`),
   KEY `k_id_probabilidad` (`k_id_probabilidad`),
   KEY `k_id_impacto` (`k_id_impacto`),
   CONSTRAINT `FK__impacto` FOREIGN KEY (`k_id_impacto`) REFERENCES `impacto` (`k_id_impacto`),
   CONSTRAINT `FK__probabilidad` FOREIGN KEY (`k_id_probabilidad`) REFERENCES `probabilidad` (`k_id_probabilidad`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.ref_probabilidad_impacto: ~25 rows (aproximadamente)
-DELETE FROM `ref_probabilidad_impacto`;
-/*!40000 ALTER TABLE `ref_probabilidad_impacto` DISABLE KEYS */;
-INSERT INTO `ref_probabilidad_impacto` (`k_⁯id_ref`, `k_id_probabilidad`, `k_id_impacto`, `n_calificacion`, `n_color`) VALUES
-	(1, 5, 5, 'Bajo', '#FFFFFF'),
-	(2, 5, 4, 'Bajo', '#FFFFFF'),
-	(3, 5, 3, 'Moderado', '#FFFFFF'),
-	(4, 5, 2, 'Alto', '#FFFFFF'),
-	(5, 5, 1, 'Extremo', '#FFFFFF'),
-	(6, 4, 5, 'Bajo', '#FFFFFF'),
-	(7, 4, 4, 'Bajo', '#FFFFFF'),
-	(8, 4, 3, 'Moderado', '#FFFFFF'),
-	(9, 4, 2, 'Alto', '#FFFFFF'),
-	(10, 4, 1, 'Extremo', '#FFFFFF'),
-	(11, 3, 5, 'Bajo', '#FFFFFF'),
-	(12, 3, 4, 'Moderado', '#FFFFFF'),
-	(13, 3, 3, 'Moderado', '#FFFFFF'),
-	(14, 3, 2, 'Alto', '#FFFFFF'),
-	(15, 3, 1, 'Extremo', '#FFFFFF'),
-	(16, 2, 5, 'Bajo', '#FFFFFF'),
-	(17, 2, 4, 'Moderado', '#FFFFFF'),
-	(18, 2, 3, 'Alto', '#FFFFFF'),
-	(19, 2, 2, 'Alto', '#FFFFFF'),
-	(20, 2, 1, 'Extremo', '#FFFFFF'),
-	(21, 1, 5, 'Bajo', '#FFFFFF'),
-	(22, 1, 4, 'Moderado', '#FFFFFF'),
-	(23, 1, 3, 'Alto', '#FFFFFF'),
-	(24, 1, 2, 'Extremo', '#FFFFFF'),
-	(25, 1, 1, 'Extremo', '#FFFFFF');
-/*!40000 ALTER TABLE `ref_probabilidad_impacto` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.riesgo
 DROP TABLE IF EXISTS `riesgo`;
 CREATE TABLE IF NOT EXISTS `riesgo` (
@@ -283,17 +180,7 @@ CREATE TABLE IF NOT EXISTS `riesgo` (
   PRIMARY KEY (`k_id_riesgo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.riesgo: ~5 rows (aproximadamente)
-DELETE FROM `riesgo`;
-/*!40000 ALTER TABLE `riesgo` DISABLE KEYS */;
-INSERT INTO `riesgo` (`k_id_riesgo`, `n_riesgo`, `n_riesgo_descripcion`, `n_responsable`) VALUES
-	('a', 'a', NULL, NULL),
-	('b', 'b', NULL, NULL),
-	('c', 'c', NULL, NULL),
-	('d', 'd', NULL, NULL),
-	('e', 'e', NULL, NULL);
-/*!40000 ALTER TABLE `riesgo` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.riesgo_actividad
 DROP TABLE IF EXISTS `riesgo_actividad`;
 CREATE TABLE IF NOT EXISTS `riesgo_actividad` (
@@ -307,11 +194,7 @@ CREATE TABLE IF NOT EXISTS `riesgo_actividad` (
   CONSTRAINT `fk_fk_ra_riesgo` FOREIGN KEY (`k_id_riesgo`) REFERENCES `riesgo` (`k_id_riesgo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.riesgo_actividad: ~0 rows (aproximadamente)
-DELETE FROM `riesgo_actividad`;
-/*!40000 ALTER TABLE `riesgo_actividad` DISABLE KEYS */;
-/*!40000 ALTER TABLE `riesgo_actividad` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.riesgo_especifico
 DROP TABLE IF EXISTS `riesgo_especifico`;
 CREATE TABLE IF NOT EXISTS `riesgo_especifico` (
@@ -338,13 +221,9 @@ CREATE TABLE IF NOT EXISTS `riesgo_especifico` (
   CONSTRAINT `fk_fk_re_riesgo` FOREIGN KEY (`k_id_riesgo`) REFERENCES `riesgo` (`k_id_riesgo`),
   CONSTRAINT `fk_fk_re_tipo_evento` FOREIGN KEY (`k_id_tipo_evento_2`) REFERENCES `tipo_evento_2` (`k_id_tipo_evento_2`),
   CONSTRAINT `fk_fk_re_zona_geografica` FOREIGN KEY (`k_id_zona_geografica`) REFERENCES `zona_geografica` (`k_id_zona_geografica`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.riesgo_especifico: ~0 rows (aproximadamente)
-DELETE FROM `riesgo_especifico`;
-/*!40000 ALTER TABLE `riesgo_especifico` DISABLE KEYS */;
-/*!40000 ALTER TABLE `riesgo_especifico` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.soporte
 DROP TABLE IF EXISTS `soporte`;
 CREATE TABLE IF NOT EXISTS `soporte` (
@@ -361,36 +240,18 @@ CREATE TABLE IF NOT EXISTS `soporte` (
   CONSTRAINT `FK_soporte_riesgo_especifico` FOREIGN KEY (`k_id_riesgo_especifico`) REFERENCES `riesgo_especifico` (`k_id_riesgo_especifico`),
   CONSTRAINT `fk_fk_so_impacto` FOREIGN KEY (`k_id_impacto`) REFERENCES `impacto` (`k_id_impacto`),
   CONSTRAINT `fk_fk_so_probabilidad` FOREIGN KEY (`k_id_probabilidad`) REFERENCES `probabilidad` (`k_id_probabilidad`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.soporte: ~0 rows (aproximadamente)
-DELETE FROM `soporte`;
-/*!40000 ALTER TABLE `soporte` DISABLE KEYS */;
-/*!40000 ALTER TABLE `soporte` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.tipo_evento_1
 DROP TABLE IF EXISTS `tipo_evento_1`;
 CREATE TABLE IF NOT EXISTS `tipo_evento_1` (
   `k_id_tipo_evento_1` int(11) NOT NULL AUTO_INCREMENT,
   `n_descripcion` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`k_id_tipo_evento_1`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.tipo_evento_1: ~9 rows (aproximadamente)
-DELETE FROM `tipo_evento_1`;
-/*!40000 ALTER TABLE `tipo_evento_1` DISABLE KEYS */;
-INSERT INTO `tipo_evento_1` (`k_id_tipo_evento_1`, `n_descripcion`) VALUES
-	(1, '1. Fraude Interno'),
-	(2, '2. Fraude Externo'),
-	(3, '3. Relaciones Laborales'),
-	(4, '5. Daños a activos físicos'),
-	(5, '6. Fallas tecnológicas'),
-	(6, '7. Ejecución y administración de procesos'),
-	(7, '8. Lavado de Activos'),
-	(8, '9. Reputacional'),
-	(9, '10. Legal');
-/*!40000 ALTER TABLE `tipo_evento_1` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.tipo_evento_2
 DROP TABLE IF EXISTS `tipo_evento_2`;
 CREATE TABLE IF NOT EXISTS `tipo_evento_2` (
@@ -400,36 +261,9 @@ CREATE TABLE IF NOT EXISTS `tipo_evento_2` (
   PRIMARY KEY (`k_id_tipo_evento_2`),
   KEY `fk_fk_te2_tipo_evento` (`k_id_tipo_evento_1`),
   CONSTRAINT `fk_fk_te2_tipo_evento` FOREIGN KEY (`k_id_tipo_evento_1`) REFERENCES `tipo_evento_1` (`k_id_tipo_evento_1`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.tipo_evento_2: ~22 rows (aproximadamente)
-DELETE FROM `tipo_evento_2`;
-/*!40000 ALTER TABLE `tipo_evento_2` DISABLE KEYS */;
-INSERT INTO `tipo_evento_2` (`k_id_tipo_evento_2`, `k_id_tipo_evento_1`, `n_descripcion`) VALUES
-	(1, 1, '1.1. Actividades no autorizadas'),
-	(2, 1, '1.2. Hurto y Fraude'),
-	(3, 2, '2.1. Hurto y Fraude'),
-	(4, 2, '2.2. Vulnerabilidad  de los sistemas'),
-	(5, 3, '3.1. Fallas en las Relaciones laborales'),
-	(6, 3, '3.2. Fallas en la seguridad del entorno laboral'),
-	(7, 3, '3.3. Discriminación'),
-	(8, 4, '4.1. Administración indebida  de activos y revelac'),
-	(9, 4, '4.2. Prácticas inapropiadas de negocios o de merca'),
-	(10, 4, '4.3. Fallas en los productos'),
-	(11, 4, '4.4. Fallas en la selección y gerenciamiento de lo'),
-	(12, 4, '4.5. Fallas en la asesoría a los clientes'),
-	(13, 5, '5.1. Desastres y otros eventos'),
-	(14, 6, '6.1. Fallas en los Sistemas'),
-	(15, 7, '7.1.1 Fallas en el diseño de los procesos. '),
-	(16, 7, '7.1.2 Fallas en la ejecución de los procesos.   '),
-	(17, 7, '7.1.3 Fallas en el mantenimiento de los procesos.'),
-	(18, 7, '7.2. Inoportunidad o inexactitud en la generación '),
-	(19, 7, '7.3. Ausencia de  documentación o documentación in'),
-	(20, 7, '7.4. Inadecuada administración de las cuentas de c'),
-	(21, 7, '7.5. Fallas de contrapartes comerciales'),
-	(22, 7, '7.6. Fallas de Proveedores o Outsourcing');
-/*!40000 ALTER TABLE `tipo_evento_2` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.usuarios
 DROP TABLE IF EXISTS `usuarios`;
 CREATE TABLE IF NOT EXISTS `usuarios` (
@@ -443,26 +277,21 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `n_password` varchar(30) DEFAULT NULL,
   `n_rol_ususario` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`k_id_usuarios`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.usuarios: ~0 rows (aproximadamente)
-DELETE FROM `usuarios`;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla matriz_riesgo.zona_geografica
 DROP TABLE IF EXISTS `zona_geografica`;
 CREATE TABLE IF NOT EXISTS `zona_geografica` (
   `k_id_zona_geografica` int(11) NOT NULL AUTO_INCREMENT,
   `n_nombre` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`k_id_zona_geografica`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla matriz_riesgo.zona_geografica: ~0 rows (aproximadamente)
-DELETE FROM `zona_geografica`;
-/*!40000 ALTER TABLE `zona_geografica` DISABLE KEYS */;
-/*!40000 ALTER TABLE `zona_geografica` ENABLE KEYS */;
-
+-- La exportación de datos fue deseleccionada.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+
+
